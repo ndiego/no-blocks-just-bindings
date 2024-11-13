76bb9e52f0325237ba9304a5b7763dc0e06ac409
Nick Diego <nickmdiego@gmail.com>
2024-11-13 09:40:35 -0500

    Fresh build.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks', 'wp-i18n', 'wp-primitives', 'wp-wordcount'), 'version' => 'ccb2d32aeef1697e259e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks', 'wp-i18n', 'wp-primitives', 'wp-wordcount'), 'version' => '1aae812a427a38bbc801');
